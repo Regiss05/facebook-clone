c497ee6a6ea72f0aed3ad52247c500fd3fdc901b
regiss05 <regiss.steven@gmail.com>
2023-03-04 09:59:46 +0200

    complete google auth

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BsFacebook } from 'react-icons/bs'
 import { FcGoogle } from 'react-icons/fc'
+import { signIn } from 'next-auth/react'
 
 const Login = () => {
     return (
@@ -9,7 +10,7 @@ const Login = () => {
                 <BsFacebook className='text-white text-[200px]' />
             </div>
             <div className='grid place-items-center bg-black'>
-                <div className='flex gap-4 bg-[#fff] p-4 px-6 items-center rounded-[6px] cursor-pointer'>
+                <div className='flex gap-4 bg-[#fff] p-4 px-6 items-center rounded-[6px] cursor-pointer' onClick={()=> signIn('google')}>
                     <FcGoogle className='text-[30px]' />
                     SignIn With Google
                 </div>
